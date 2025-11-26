9ddc4648b21cdce65abdcf96b78bbc828ed99846
Hien110 <minhhien30201@gmail.com>
2025-11-27 02:11:28 +0700

    feat rating supporter

--- src/routes/rating.routes.js
+++ src/routes/rating.routes.js
@@ -7,6 +7,21 @@ const authenticateToken = require("../app/middlewares/authMiddleware");
 
 
 router.use(authenticateToken);
+
+// Đánh giá dịch vụ hỗ trợ
+router.post("/", RatingController.createRatingSupportService);
+
+// Lấy đánh giá theo Id dịch vụ hỗ trợ
+router.get("/service-support/:serviceSupportId/:reviewer", RatingController.getRatingsByServiceSupportIdAndReviewer);
+
+// Chỉnh sửa: Lấy đánh giá theo Id người dùng được đánh giá
+router.put("/:ratingId", RatingController.updateRatingById);
+
+// Xóa: Lấy đánh giá theo Id người dùng được đánh giá
+router.delete("/:ratingId", RatingController.deleteRatingById);
+
+// Lấy đánh giá theo Id người dùng được đánh giá
 router.get("/:userId", RatingController.getRatingsByUserId);
+
 
 module.exports = router;
